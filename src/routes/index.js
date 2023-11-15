ee68fe0da8de11c36b0b445bfac0578ce72a6e31
Padmavathi Manoharan <padmam725@gmail.com>
2023-11-15 16:10:20 -0500

    add initial route message

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,9 @@ import todoRoutes from './todo.routes.js'
 
 const router = express.Router()
 
+router.get('/', function(req, res) {
+    res.send('Hello World')
+})
 router.use('/todo', todoRoutes)
 
 export default router
